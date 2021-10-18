a34bb6d73f0860c69e79f9a2a29187accbc6e0a6
dron11231 <astar.dron@gmail.com>
2021-10-18 19:37:12 +0300

    The switch is working correctly

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
-let ops = document.querySelectorAll('.operator');
 let nums = document.querySelectorAll('.number');
 let num
 let output = document.getElementById('calc-display');
+let ops = document.querySelectorAll('.operator');
 
 let cleaner = function(input) {
     input.value = "";
+    ops.forEach(elem => {
+        elem.classList.remove('on');
+    })
 
 }
 
@@ -23,28 +26,16 @@ let deactivator = function(notActive1, notActive2, notActive3){
     notActive3.className = "off";
 }
 
-function toggleState(item){
-    if(item.className == "on") {
-       item.className="off";
-       
-    } else {
-       item.className="on";
-       switch (item) {
-           case plus:
-               deactivator(minus, multiply, split);
-               break;
-            case minus:
-               deactivator(plus, multiply, split);
-               break;
-            case multiply:
-               deactivator(minus, plus, split);
-               break;
-            case split:
-               deactivator(minus, multiply, plus);
-               break;
-       }
-    };
-    let itemsOn = document.querySelectorAll('.on');
-    
- }
-
+let toggleState = function (item) {
+    item.classList.remove('operator') 
+    if (item.className == 'off') {
+        ops.forEach(el => {
+            el.className = 'off';
+            item.className = 'on';
+        })
+    }
+    else {
+        item.className = 'off'
+    }
+    console.log(ops);
+}
